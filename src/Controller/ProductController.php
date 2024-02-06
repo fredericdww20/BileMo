0dc7181c3586ecf9d75a32626f8423fe6bb67a1f
Frederic Portemer <synapsefred@hotmail.fr>
2024-02-06 15:49:37 +0100

    Update ProductController.php

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,17 +5,30 @@ namespace App\Controller;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
     #[Route('api/product', name: 'app_product')]
-    public function getAllProduct(ProductRepository $productRepository): JsonResponse
+    public function getAllProduct(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
     {
         $productlist = $productRepository->getAllProduct();
 
-        return new JsonResponse([
-            'products' => $productlist
-        ]);
+        $jsonproductlist = $serializer->serialize($productlist, 'json');
+        return new JsonResponse($jsonproductlist, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('api/product/{id}', name: 'app_product_id')]
+    public function getProductById(ProductRepository $productRepository, SerializerInterface $serializer, int $id): JsonResponse
+    {
+     $product = $productRepository->getProductById($id);
+     if ($product) {
+         $jsonproduct = $serializer->serialize($product, 'json');
+         return new JsonResponse($jsonproduct, Response::HTTP_OK, [], true);
+     }
+     
+     return new JsonResponse('Product not found', Response::HTTP_NOT_FOUND);
     }
 }
