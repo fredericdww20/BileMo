d08597cda83164a983ba80a609ee54f2c9390861
Frederic Portemer <synapsefred@hotmail.fr>
2024-02-27 15:35:13 +0100

    Update Client.php

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -121,15 +121,5 @@ class Client
         return $this;
     }
 
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): static
-    {
-        $this->password = $password;
-
-        return $this;
-    }
+    
 }
