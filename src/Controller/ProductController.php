534a4552c57542cc8ec9ce158cd38c684456e705
Frederic Portemer <synapsefred@hotmail.fr>
2024-06-12 22:17:18 +0200

    Update ProductController.php

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -203,7 +203,7 @@ class ProductController extends AbstractController
      * @OA\Tag(name="Product")
      * @Security(name="Bearer")
      */
-    #[Route('api/createproduct', name: 'app_product_create', methods: ['POST'])]
+    #[Route('api/products', name: 'app_product_create', methods: ['POST'])]
     public function createProduct(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
